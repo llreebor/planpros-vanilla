1d1edf4c49f4ccef4fd1b6deaa75b02099534418
mac <mac@Roman-Timoshchuk.local>
2024-05-22 20:20:53 +0300

    init

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+new Swiper('.testimonial__slider', {
+	// Responsive breakpoints
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		640: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+		1040: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+	},
+})
+
+new Swiper('.testimonial__slider-2', {
+	// Responsive breakpoints
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		640: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+		1040: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+	},
+})
+
+new Swiper('.testimonial__slider-3', {
+	// Responsive breakpoints
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		640: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+		1040: {
+			slidesPerView: 3,
+			spaceBetween: 40,
+		},
+	},
+})
